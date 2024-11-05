561fd7bb05e89d83472db7a3592ed64214069ee4
Eoin Treacy <eointreacy5@gmail.com>
2024-11-05 19:19:37 +0000

    [feat] handle new and custom item entry and deletion

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Products() {
     interface Item {
@@ -9,11 +9,24 @@ export default function Products() {
 
     const [data, setData] = useState<Item[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
+    const [newItem, setNewItem] = useState<string>("Spaghetti");
 
-    function handleClick() {
-        const newItem: Item = { id: data.length + 1, name: "Spaghetti" };
-        const newData: Item[] = [...data, newItem];
+    function addItem() {
+        if (newItem === "") return;
+        const item: Item = { id: data.length + 1, name: newItem };
+        const newData: Item[] = [...data, item];
         setData(newData);
+        setNewItem("");
+    }
+    function deleteItem() {
+        // delete last itm in data
+        const newData: Item[] = [...data];
+        newData.pop();
+        setData(newData);
+    }
+
+    function handleItemChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setNewItem(event.target.value);
     }
 
     useEffect(() => {
@@ -35,12 +48,23 @@ export default function Products() {
             <ul aria-label="Product List">
                 {data.map((item: Item) => <li key={item.id}>{item.name}</li>)}
             </ul>
+            <input 
+                type="text" 
+                aria-label="Enter New Item Field" 
+                value={newItem}
+                onChange={handleItemChange}
+            />
             <button
-                onClick={handleClick}
-                value="Spaghetti"
-                aria-label="Add Spaghetti"
+                onClick={addItem}
+                aria-label="Add Item"
             >
-                Add Spaghetti
+                Add Item
+            </button>
+            <button
+                onClick={deleteItem}
+                aria-label="Delete Item"
+            >
+                Delete Item
             </button>
         </div>
     );
