6b6223c827951bae155497617f52b55f2ddb9a52
Eoin Treacy <eointreacy5@gmail.com>
2024-11-05 18:46:21 +0000

    [refactor] more types, spread syntax, async-await, seperation of logic from view

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -2,46 +2,45 @@
 import { useEffect, useState } from "react";
 
 export default function Products() {
-  
-
     interface Item {
-      id: number;
-      name: string;
-  
-  }
-    const [data, setData] = useState<any>([]);
-  
-    useEffect(() => {
-      console.log("fetching data");
-      // as suspected this calls every time the component is rendered
-      // it's WAS missing the dependency array
-      fetch('products.json')
-        .then(response => response.json())
-        .then(json => setData(json));
-    }, []);
-  
-    if (!data) {
-      return <p>Loading...</p>;
+        id: number;
+        name: string;
     }
-  
+
+    const [data, setData] = useState<Item[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    function handleClick() {
+        const newItem: Item = { id: data.length + 1, name: "Spaghetti" };
+        const newData: Item[] = [...data, newItem];
+        setData(newData);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response: Response = await fetch("products.json");
+            const items: Item[] = await response.json();
+            setData(items);
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <div>
-      <ul>
-        {(data as Item[]).map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-      <button onClick={()=> {
-        const newdata =  data.concat({id:"5", name: "Spaghetti"}); 
-        setData(newdata)
-        console.log(newdata);
-        
-      }} 
-        value="Spaghetti">
-      Add Spaghetti
-    </button>
-    </div>      
+            <ul>
+                {data.map((item: Item) => <li key={item.id}>{item.name}</li>)}
+            </ul>
+            <button
+                onClick={handleClick}
+                value="Spaghetti"
+            >
+                Add Spaghetti
+            </button>
+        </div>
     );
-  }
-
+}
